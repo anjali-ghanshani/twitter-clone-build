9e64e611375eace2799e33ba45248874d1b2e4c6
anjali-ghanshani <anjali.ghanshani@gmail.com>
2022-08-02 20:56:18 +1000

    implement the Image URL functinality

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -6,14 +6,34 @@ import {
   PhotographIcon,
   SearchCircleIcon,
 } from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
 
 function Tweetbox() {
   const [input, setInput] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const { data: session } = useSession();
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (!imageInputRef.current?.value) return;
+
+    setImage(imageInputRef.current.value);
+    imageInputRef.current.value = "";
+    setImageUrlBoxIsOpen(false);
+  };
+
   return (
     <div className="flex sapce-x-2 p-5">
       <img
         className="h-14 w-14 object-cover rounded-full cursor-pointer mt-4"
-        src="https://links.papareact.com/gll"
+        src={session?.user?.image || "https://links.papareact.com/gll"}
         alt=""
       />
 
@@ -21,26 +41,57 @@ function Tweetbox() {
         <form className="flex flex-1 flex-col">
           <input
             value={input}
-            onChange={e => {setInput(e.target.value)}}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
             type="text"
             placeholder="What's Happening?"
             className="h-24 w-full text-xl outline-none placeholder:text-xl"
           />
           <div className="flex items-center">
             <div className="flex space-x-2 flex-1 text-twitter">
-              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <PhotographIcon
+                onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+                className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+              />
               <SearchCircleIcon className="h-5 w-5" />
               <EmojiHappyIcon className="h-5 w-5" />
               <CalendarIcon className="h-5 w-5" />
               <LocationMarkerIcon className="h-5 w-5" />
             </div>
 
-            <button 
-              disabled={!input}
-              className="bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40">
+            <button
+              disabled={!input || !session}
+              className="bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40"
+            >
               Tweet
             </button>
           </div>
+          {imageUrlBoxIsOpen && (
+            <form className="rounded-lg mt-5 flex bg-twitter/80 py-2 px-4">
+              <input
+                ref={imageInputRef}
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white "
+                type="text"
+                placeholder="Enter Image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+
+          {image && (
+            <img
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
+              src={image}
+              alt=""
+            />
+          )}
         </form>
       </div>
     </div>
