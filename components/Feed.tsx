bbfae1410007fa325596af587409d3ef28f2b9d8
anjali-ghanshani <anjali.ghanshani@gmail.com>
2022-08-02 10:05:11 +1000

    React Hot Toast Implementation

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,6 +4,7 @@ import { Tweet } from "../typings";
 import Tweetbox from "./Tweetbox";
 import TweetComponent from "../components/Tweet";
 import { fetchTweets } from "../utils/fetchTweets";
+import toast from "react-hot-toast";
 
 interface Props {
   tweets: Tweet[];
@@ -13,8 +14,14 @@ function Feed({ tweets: tweetsProp }: Props) {
   const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
 
   const handleRefresh = async () => {
+    const refreshToast = toast.loading('Refeshing...');
+
     const tweets = await fetchTweets();
     setTweets(tweets);
+
+    toast.success('Feed Updated!', {
+      id:refreshToast
+    })
   };
 
   return (
