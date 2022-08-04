fc9ceabf19e20a69c8f41f65a28b72894931ac35
anjali-ghanshani <anjali.ghanshani@gmail.com>
2022-08-04 15:49:10 +1000

    added next-auth GoogleProvider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
+import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),  
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
